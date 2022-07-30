1296c8d7a145e99aaf408a0d985fe9f1dce4c47f
Henry <henry194557@gmail.com>
2022-07-30 13:40:39 +0800

    change

--- sum.js
+++ sum.js
@@ -1,8 +1,24 @@
-function sum(n){
-    add=(n*(n+1)/2)
-    return add
+function sum1(n) {
+    return (n * (n + 1) / 2);
 }
 
-console.log(sum(1)); // 1
-console.log(sum(3)); // 6
-console.log(sum(10)); // 55
+function sum2(n) {
+    let result = 0;
+    for (let i = 1; i <= n; i++) {
+        result += i;
+    }
+    return result;
+}
+
+function sum3(n) {
+    if (n === 1) {
+        return n;
+    }
+    return sum (n-1) + n;
+}
+
+console.log(sum1(1)); // 1
+console.log(sum1(3)); // 6
+console.log(sum1(10)); // 55
+
+
