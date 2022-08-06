211a1c84143c8f8dd07f34d2f705b33524844aed
Henry <henry194557@gmail.com>
2022-08-06 15:13:37 +0800

    add

---
+++ promise/promise1.js
@@ -0,0 +1,13 @@
+function doWork(job, timer, cb) {
+
+  new Promise();
+  
+    // 為了模擬一個非同步工作
+    setTimeout(() => {
+      let dt = new Date();
+      // callback 慣用設計：
+      // 第一個參數: error
+      // 第二個參數: 要回覆的資料
+      cb(null, `完成工作 ${job} at ${dt.toISOString()}`);
+    }, timer);
+}
