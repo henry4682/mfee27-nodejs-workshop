c956e007ff6fe4a3d64e2ad74fcb7da01c4fdef0
Henry <henry194557@gmail.com>
2022-08-13 09:57:30 +0800

    add file

---
+++ promise/order1.js
@@ -0,0 +1,14 @@
+async function test() {
+    console.log(1);
+    await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(2);
+            resolve();
+        },0);
+    });
+    console.log(3);
+}
+
+console.log(4)
+test();
+console.log(5)
