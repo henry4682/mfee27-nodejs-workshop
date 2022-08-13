fda8a65f6a7bcdb0666f3aad2b4349b9e7ebcbb6
Henry <henry194557@gmail.com>
2022-08-13 10:04:00 +0800

    completed file

--- promise/readfile.js
+++ promise/readfile.js
@@ -1,1 +1,9 @@
-fs.readFile()
+const fs =require('fs')
+
+fs.readFile('test.txt','utf8',(err , data) => {
+    if(err){
+        console.error(err)
+    } else {
+        console.log(data)
+    }
+})
